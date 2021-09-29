78ac11f3199149901d30a39640cca662e3525659
Jay Thesia <jayengineer.jp2410@gmail.com>
2021-09-29 13:27:28 +0530

    Add files via upload

---
+++ 4_Odd_Even/4_ODD_EVEN.js
@@ -0,0 +1,7 @@
+let number = prompt("Enter you number:");
+
+if (number % 2 == 0) {
+    document.write("The number is " + number + " EVEN");
+} else {
+    document.write("The number is " + number + " ODD");
+}
