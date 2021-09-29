6c8399372fea3eb8e1edb3c2f2f585617f2a13f3
Jay Thesia <jayengineer.jp2410@gmail.com>
2021-09-29 13:52:07 +0530

    Add files via upload

---
+++ 5. today's_date.js
@@ -0,0 +1,12 @@
+let today = new Date();
+
+let time = today.getTime();
+
+//current day in week
+let day = today.getDay();
+
+//current date
+let date = today.getDate();
+console.log(time);
+console.log(day);
+console.log(date);
